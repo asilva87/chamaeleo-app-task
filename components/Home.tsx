f5c7d70aa4e7ffe40b27b2d0546273c1d9ea88ad
André Silva <dev01766@proton.me>
2024-05-29 17:12:42 +0200

    Move Home page to own component. Add more content and logic.

---
+++ components/Home.tsx
@@ -0,0 +1,44 @@
+import { Button, Spinner, Text, YStack } from 'tamagui'
+
+interface HomeProps {
+  isCheckingIn: boolean
+  isConnected: boolean
+  onCheckIn: () => void
+}
+
+export default function Home({
+  isCheckingIn,
+  isConnected,
+  onCheckIn,
+}: HomeProps): JSX.Element {
+  return (
+    <YStack alignItems="center" justifyContent="center" flex={1}>
+      <Button
+        backgroundColor={
+          !isCheckingIn
+            ? isConnected
+              ? '$blue9Light'
+              : '$orange9Light'
+            : '$gray6Light'
+        }
+        onPress={onCheckIn}
+        disabled={isCheckingIn}
+        width="$15"
+        pressStyle={{
+          backgroundColor: isConnected ? '$blue8Light' : '$orange8Light',
+        }}
+      >
+        {isCheckingIn && (
+          <Spinner
+            size="small"
+            color={isConnected ? '$blue8Light' : '$orange8Light'}
+          />
+        )}
+
+        <Text fontSize="$5" color={!isCheckingIn ? 'white' : 'black'}>
+          {!isCheckingIn ? 'Check in' : 'Checking in'}
+        </Text>
+      </Button>
+    </YStack>
+  )
+}
